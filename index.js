aced24bfde428a2b20aa84ea0191e6d94c114258
sagarjain0456 <72862757+sagarjain0456@users.noreply.github.com>
2022-05-20 15:54:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// alert("sagar");
+
+
+// myfunction( ) {
+//   alert("sagar");
+// }
+
+
+
+
+// function myFunction(){
+//
+//   // alert("sagar3");
+//
+//
+//   // document.getElementById('navitems').style.color = 'green';
+//   document.getElementById("navitems").style.color = "blue";
+// }
+
+
+function myFunction() {
+
+
+document.getElementById('navitems').style.left = '0' ;
+
+document.getElementById('navitems').style.transition = 'all 0.3s ease';
+
+document.getElementById("closehide").style.display = 'inline-block';
+document.getElementById("closehide").style.transition = '3s';
+
+
+
+}
